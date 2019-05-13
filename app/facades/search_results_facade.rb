96ff6e95b474a69f1ae74042e5765dc89126063b
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-13 11:07:25 -0600

    Removed a pry

--- app/facades/search_results_facade.rb
+++ app/facades/search_results_facade.rb
@@ -1,17 +1,17 @@
 class SearchResultsFacade
+
   def initialize(search_key)
     @search_key = search_key
-  end
 
-  def conn
-    zip_code = params[:q]
     conn = Faraday.new('https://developer.nrel.gov/api/alt-fuel-stations/v1/') do |f|
-      f.headers[api_key] = ENV['NREL_API_KEY']
-      faraday.adapter Faraday.default_adapter
+      f.params['api_key'] = ENV['NREL_API_KEY']
+      f.adapter Faraday.default_adapter
     end
 
-    response = conn.get("nearest.json?fuel_type=ELEC,LPG&access=public&format=JSON&location=#{zip_code}&status=E")
+    response = conn.get("nearest.json?fuel_type=ELEC,LPG&access=public&format=JSON&location=#{@search_key}&status=E")
     results = JSON.parse(response.body)
+    # results["fuel_stations"] #provides indy station info
+    # results["total_results"] #93
   end
 
 end
