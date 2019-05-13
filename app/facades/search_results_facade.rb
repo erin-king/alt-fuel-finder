749fdd5ffd28cda2ffe074e6f66815c4067f32e8
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-13 10:52:19 -0600

    Add figaro, place key in application.yml, set to eniro variable in SearchResultsFacade

--- app/facades/search_results_facade.rb
+++ app/facades/search_results_facade.rb
@@ -6,13 +6,12 @@ class SearchResultsFacade
   def conn
     zip_code = params[:q]
     conn = Faraday.new('https://developer.nrel.gov/api/alt-fuel-stations/v1/') do |f|
-      # f.headers[api_key] = ENV['NREL_API_KEY']
-      f.params['api_key'] = 'hmstHcsk9ZqgSAQlKIlmpKenfltTwrXK9yr4d30L'
+      f.headers[api_key] = ENV['NREL_API_KEY']
       faraday.adapter Faraday.default_adapter
     end
 
     response = conn.get("nearest.json?fuel_type=ELEC,LPG&access=public&format=JSON&location=#{zip_code}&status=E")
     results = JSON.parse(response.body)
   end
-  
+
 end
